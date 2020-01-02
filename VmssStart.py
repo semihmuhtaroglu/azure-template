a2557b1b41418f62374aac5dd262bf40d05b8ed1
semihmuhtaroglu <47088775+semihmuhtaroglu@users.noreply.github.com>
2020-01-02 12:11:56 +0300

    Update VmssStart.py

--- VmssStart.py
+++ VmssStart.py
@@ -6,10 +6,10 @@ Azure Python SDK documentation : https://aka.ms/azure-python-sdk
 import os
 import sys 
 
-SUBSCRIPTION_ID = str(sys.argv[1])
-CLIENT = str(sys.argv[2])
-KEY = str(sys.argv[3])
-TENANT_ID = str(sys.argv[4])
+subscription_id = str(sys.argv[1])
+client_ids = str(sys.argv[2])
+key = str(sys.argv[3])
+tenant_id = str(sys.argv[4])
 
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
@@ -17,7 +17,7 @@ from azure.mgmt.compute import ComputeManagementClient
 resourcegroup = 'MC_VisperaResourceGroupTest_aks-vispera-test-anw_centralindia'
 vmss_name = 'aks-general-14908190-vmss'
 instance_id = '0'
-credentials = ServicePrincipalCredentials(client_id=CLIENT, secret=KEY, tenant=TENANT_ID)
+credentials = ServicePrincipalCredentials(client_id=client_ids, secret=key, tenant=tenant_id)
 client = ComputeManagementClient(credentials, subscription_id)
 vmss = client.virtual_machine_scale_set_vms.start(resourcegroup,vmss_name,instance_id,custom_headers=None)
 
