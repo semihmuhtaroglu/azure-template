152c5e5ec91fba764e80b213a1ae35196822aadd
semihmuhtaroglu <47088775+semihmuhtaroglu@users.noreply.github.com>
2020-01-02 11:56:16 +0300

    Update VmssStart.py

--- VmssStart.py
+++ VmssStart.py
@@ -6,7 +6,7 @@ Azure Python SDK documentation : https://aka.ms/azure-python-sdk
 import os
 import sys 
 
-SUBSCRİPTION_ID = str(sys.argv[1])
+SUBSCRIPTION_ID = str(sys.argv[1])
 CLIENT = str(sys.argv[2])
 KEY = str(sys.argv[3])
 TENANT_ID = str(sys.argv[4])
